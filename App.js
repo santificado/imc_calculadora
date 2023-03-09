0bf559c775205dd9d6fe6992c104735c26dcbd0e
logonrmlocal <andrelucas.12323@gmail.com>
2023-03-09 09:30:12 -0300

    a

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+
+
+export default function App(){
+  const peso = ''
+  const altura = ''
+  const [result, SetResult] = useState('')
+  const calc = () +>{}
+   var res = 'Você está obeso'
+   setResult(res)
+
+}
+{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.container}>
+          <Text>Calcule seu IMC</Text>
+          <TextInput
+          style={{height:40}}
+          placeholder = "Digite seu peso"
+          defaultValue={peso}
+        
+        />
+          <TextInput
+          style={{height:40}}
+          placeholder = "Digite sua altura"
+          defaultValue={altura}
+        
+        />
+          <Button
+           onPress={() =>{
+           calc();
+
+           }}
+           />
+           title='Calcular'
+          </View>
+
   );
 }
 
